1fa69d087d84338e15610ddd8adfa0452630d60c
fmrkc <fmarkclarence@gmail.com>
2025-08-06 23:35:20 +0800

    Update UAccount.tsx

--- src/pages/user/UAccount.tsx
+++ src/pages/user/UAccount.tsx
@@ -1,10 +1,25 @@
-import { IonButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonItem, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import React, { useState } from 'react';
 import LogoutButton from '../../components/LogoutButton';
 import { useAuth } from '../../contexts/AuthContext';
+import { logOut } from 'ionicons/icons';
 
 const Account: React.FC = () => {
-    const { currentUser } = useAuth();
+    const { logout, currentUser } = useAuth();
+    const router = useIonRouter();
+    const [showLoading, setShowLoading] = useState(false);
+
+    const handleLogout = async () => {
+      setShowLoading(true);
+      try {
+        await logout();
+        router.push('/admin', 'forward');
+      } catch (error) {
+        console.error('Logout error:', error);
+      } finally {
+        setShowLoading(false);
+      }
+    };
 
     return (
         <>
@@ -13,9 +28,19 @@ const Account: React.FC = () => {
                     <IonTitle>Account</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">
-            {currentUser?.email}
-            <LogoutButton />
+            <IonContent >
+            <IonCard>
+                <IonCardContent>
+                    <h1>{currentUser?.email}</h1>
+                    
+                    <IonItem detail={false} button onClick={handleLogout}>
+                                  <IonIcon slot="start" icon={logOut} />
+                                  Logout
+                                </IonItem>
+                </IonCardContent>
+            </IonCard>
+
+            
           
             </IonContent>
         </>
