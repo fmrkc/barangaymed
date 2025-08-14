0df20f1af5ac2c4efece82e1acc8b264ca0b3821
fmrkc <fmarkclarence@gmail.com>
2025-08-14 12:13:41 +0800

    Updated custom font implementation

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ import '@ionic/react/css/display.css';
 import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
-import './theme/variables.css';
 import './theme/variables.scss';
+import './theme/global.scss';
 
 import './firebaseConfig';
 import { AuthProvider } from './contexts/AuthContext';
