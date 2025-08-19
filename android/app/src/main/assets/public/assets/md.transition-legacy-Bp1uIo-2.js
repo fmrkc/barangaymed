27425429c1532ec521c08dd17d98978873ad9bcc
fmrkc <fmarkclarence@gmail.com>
2025-08-20 00:06:40 +0800

    Added capacitor push notifications for future use

--- android/app/src/main/assets/public/assets/md.transition-legacy-Bp1uIo-2.js
+++ android/app/src/main/assets/public/assets/md.transition-legacy-Bp1uIo-2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Beh07Lfj.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-gNrbA3Lg.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.z,i=e.y}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
