04e6afd918bb5ac2279b297e0cfd36fde4298b78
eiseuu25 <viscayno.jefferson.jv@gmail.com>
2025-10-02 17:54:35 +0800

    02/10/25 update

--- src/pages/user/user-menu.tsx
+++ src/pages/user/user-menu.tsx
@@ -6,7 +6,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import Home from './user-home';
 import Notifications from './user-notifications';
 import User_Requests from './user-requests';
-import UserTeleRequests from './user-tele-request';
+import UserTeleRequestList from './user-tele-request-list';
 import Account from './user-my-account';
 import Announcements from './user-brgy-announcements';
 import { clipboard, home, notifications, person } from 'ionicons/icons';
@@ -46,7 +46,7 @@ const UserDashboard: React.FC = () => {
         <IonRouterOutlet>
             <Route path="/user/dashboard/home" component={Home} exact />
             <Route path="/user/dashboard/requests" component={User_Requests} exact />
-            <Route path="/user/dashboard/requests/teleconsultation-requests" component={UserTeleRequests} exact />
+            <Route path="/user/dashboard/requests/teleconsultation-requests" component={UserTeleRequestList} exact />
             <Route path="/user/dashboard/notifications" component={Notifications} exact />
             <Route path="/user/dashboard/account" component={Account} exact />
 
