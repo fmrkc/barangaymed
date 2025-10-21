e92796c4cd64df5ae736709f1dae9d86f1b09e59
fmrkc <fmarkclarence@gmail.com>
2025-10-21 23:10:26 +0800

    sa-med-inv updates
    - added search bar
    - added refresher
    - added message when no medicines found
    - added placeholder filter button
    - added placeholder dropdowns
    - added view details modal when clicking on medicine
    - added audit trail
    - added placeholder transactions

--- src/types/medicine.ts
+++ src/types/medicine.ts
@@ -1,3 +1,20 @@
+import { Timestamp } from 'firebase/firestore';
+
+export interface AuditTrailEntry {
+  action: string;
+  userId: string;
+  userEmail: string;
+  userName: string;
+  timestamp: Date;
+}
+
+export interface FirestoreAuditTrailEntry {
+  action: string;
+  userId: string;
+  userEmail: string;
+  userName: string;
+  timestamp: Timestamp;
+}
 export interface Medicine {
   id?: string;
   medicine_name: string;
@@ -11,4 +28,5 @@ export interface Medicine {
   unit_name: string;
   conversion_factor: number;
   quantity: number;
+  auditTrail?: AuditTrailEntry[];
 }
