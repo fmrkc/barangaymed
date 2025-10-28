d23f76ba68240fe8f17e882b15fdf75be2ccbe3b
fmrkc <fmarkclarence@gmail.com>
2025-10-28 21:36:31 +0800

    fixed page structure of intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -20,59 +20,60 @@ const SwiperButtonNext = ({ children }:any) => {
 
 const Intro: React.FC<ContainerProps> = ({ onFinish }) => {
     return (
-        <div className="intro-container">
-        <IonHeader className='ion-no-border'>
-            <IonToolbar className='ion-text-center'>
-                <IonText color='primary'><b>BarangayMed+</b></IonText>
-            </IonToolbar>
-        </IonHeader>
-           <Swiper
-               modules={[Autoplay]}
-               autoplay={{ delay: 3000, disableOnInteraction: false }}
-               loop={true}
-               className="swiper"
-           >
-            <SwiperSlide>
-                
-               
-                <h2>Request medicines <br /> <IonText color={'primary'}> <b>straight from your phone.</b></IonText></h2>
+        <IonPage>
+            <IonHeader className='ion-no-border'>
+                <IonToolbar className='ion-text-center'>
+                    <IonText color='primary'><b>BarangayMed+</b></IonText>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="intro-container">
+                <Swiper
+                    modules={[Autoplay]}
+                    autoplay={{ delay: 3000, disableOnInteraction: false }}
+                    loop={true}
+                    className="swiper"
+                >
+                    <SwiperSlide>
 
-               
-            </SwiperSlide>
-        <SwiperSlide>
-           
-            <h2>Book appointments <br /> <IonText color={'primary'}><b>in only a few taps.</b></IonText></h2>
-           
-            
-        </SwiperSlide>
-        <SwiperSlide>
-            
-            <h2>Access barangay announcements <br /> <IonText color={'primary'}><b>in real time.</b></IonText></h2>
-           
-           
-        </SwiperSlide>
-        <SwiperSlide>
-           
-            <h1><IonText color={'primary'}>BarangayMed+</IonText> </h1>
-            <h2>Get access to medicine, book appointments, and see real-time barangay announcements <br /> <IonText color={'primary'}><b>all in one app!</b></IonText> </h2>
-            
-            </SwiperSlide>
-       </Swiper>
+
+                        <h2>Request medicines <br /> <IonText color={'primary'}> <b>straight from your phone.</b></IonText></h2>
+
+
+                    </SwiperSlide>
+                    <SwiperSlide>
+
+                        <h2>Book appointments <br /> <IonText color={'primary'}><b>in only a few taps.</b></IonText></h2>
+
+
+                    </SwiperSlide>
+                    <SwiperSlide>
+
+                        <h2>Access barangay announcements <br /> <IonText color={'primary'}><b>in real time.</b></IonText></h2>
+
+
+                    </SwiperSlide>
+                    <SwiperSlide>
+
+                        <h1><IonText color={'primary'}>BarangayMed+</IonText> </h1>
+                        <h2>Get access to medicine, book appointments, and see real-time barangay announcements <br /> <IonText color={'primary'}><b>all in one app!</b></IonText> </h2>
+
+                    </SwiperSlide>
+                </Swiper>
+            </IonContent>
             <IonFooter>
                 <IonToolbar>
                     <IonButton
-                    shape='round'
-                    expand='block'
-                    className='ion-padding-vertical'
-                    onClick={() => onFinish()}
+                        shape='round'
+                        expand='block'
+                        className='ion-padding-vertical'
+                        onClick={() => onFinish()}
                     >
-                       <IonText color={'light'}> <b>Get Started</b> </IonText>
-                       <IonIcon slot='end' icon={arrowForward} color='light' />
+                        <IonText color={'light'}> <b>Get Started</b> </IonText>
+                        <IonIcon slot='end' icon={arrowForward} color='light' />
                     </IonButton>
                 </IonToolbar>
             </IonFooter>
-        </div>
-     
+        </IonPage>
     );
 };
 
